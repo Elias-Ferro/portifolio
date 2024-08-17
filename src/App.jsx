9099fef3950b36c8800c64b7053c8c7a4c41bc72
Elias-Ferro <ferrogomes10@gmail.com>
2024-08-17 15:12:56 -0300

    estilizando cor de fundo

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import Header from "./components/Header";
 import avatarPixelArt from "../public/image/profile-pixel-art.png";
+import { Box } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
 
 const App = () => {
+  const theme = useTheme();
   const buttons = [
     { button: 1, label: "home", section: "" },
     { button: 1, label: "abount", section: "" },
     { button: 1, label: "contact", section: "" },
   ];
-  return <Header urlAvatarImage={avatarPixelArt} listButtons={buttons} />;
+  return (
+    <Box
+      sx={{
+        backgroundColor: theme.palette.background.default,
+        minHeight: "100vh",
+      }}
+    >
+      <Header urlAvatarImage={avatarPixelArt} listButtons={buttons} />;
+    </Box>
+  );
 };
 
 export default App;
