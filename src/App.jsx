129ced865311ad0b277127a7b2efda63c54320ea
Elias-Ferro <ferrogomes10@gmail.com>
2024-08-17 14:42:08 -0300

    criado tema inicial do projeto

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import avatarPixelArt from "../public/image/profile-pixel-art.png";
 
 const App = () => {
   const buttons = [
-    { button: 1, label: "home", section: '' },
-    { button: 1, label: "abount", section: '' },
-    { button: 1, label: "contact", section: '' },
+    { button: 1, label: "home", section: "" },
+    { button: 1, label: "abount", section: "" },
+    { button: 1, label: "contact", section: "" },
   ];
   return <Header urlAvatarImage={avatarPixelArt} listButtons={buttons} />;
 };
---
+++ src/style/theme.jsx
@@ -0,0 +1,19 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#3498db",
+    },
+    secondary: {
+      main: "#2ecc71",
+    },
+    background: {
+      default: "#ecf0f1",
+      paper: "#fff",
+    },
+    text: {
+      primary: "#2c3e50",
+    },
+  },
+});
