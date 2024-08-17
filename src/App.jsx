87763a5555779e208ecdd0325e4da737099d6a32
Elias-Ferro <ferrogomes10@gmail.com>
2024-08-16 23:13:29 -0300

    criado estrutura do cabeçalho

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import Header from "./components/Header";
+import avatarPixelArt from "../public/image/profile-pixel-art.png";
+
 const App = () => {
-  return (
-    <>
-      <p>ola mundo</p>
-    </>
-  );
+  const buttons = [
+    { button: 1, label: "home", section: '' },
+    { button: 1, label: "abount", section: '' },
+    { button: 1, label: "contact", section: '' },
+  ];
+  return <Header urlAvatarImage={avatarPixelArt} listButtons={buttons} />;
 };
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+import { AppBar, Avatar, Box, Button, Stack } from "@mui/material";
+
+const Header = ({ urlAvatarImage, listButtons }) => {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar sx={{ padding: "14px" }}>
+        <Stack direction={"row"} justifyContent={"space-between"}>
+          <Avatar alt="profile-pixel-art" src={urlAvatarImage} />
+          <Stack direction={"row"} gap={"10px"}>
+            {listButtons.map((button, index) => {
+              return (
+                <Button key={index} variant="text" sx={{ color: "#fff" }}>
+                  {button.label}
+                </Button>
+              );
+            })}
+          </Stack>
+        </Stack>
+      </AppBar>
+    </Box>
+  );
+};
+
+export default Header;
