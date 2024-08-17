39d7aa40993ffcbc404a484755a65e4c89d97075
Elias-Ferro <ferrogomes10@gmail.com>
2024-08-17 15:11:48 -0300

    importando estilos globais

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { ThemeProvider } from "@mui/material/styles";
 import { theme } from "./style/theme.jsx";
+import "../src/index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
