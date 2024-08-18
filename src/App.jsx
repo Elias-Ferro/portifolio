af208288ca5eac868346478ef03b019df9b59d95
Elias-Ferro <ferrogomes10@gmail.com>
2024-08-17 21:35:00 -0300

    criado componente para Titulo de apresentação

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import Header from "./components/Header";
 import avatarPixelArt from "../public/image/profile-pixel-art.png";
-import { Box } from "@mui/material";
+import { Avatar, Box } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
+import TitlePresentation from "./components/base/TitlePresentation";
 
 const App = () => {
   const theme = useTheme();
   const buttons = [
-    { button: 1, label: "home", section: "" },
-    { button: 1, label: "abount", section: "" },
-    { button: 1, label: "contact", section: "" },
+    { id: 1, label: "home", section: "" },
+    { id: 2, label: "abount", section: "" },
+    { id: 3, label: "contact", section: "" },
   ];
+
+  const technologies = ["Javascript", "React", "Typescript"];
+  const styles = ["Bootstrap", "Material Ui", "Tailwind CSS"];
+
   return (
     <Box
       sx={{
@@ -18,6 +23,30 @@ const App = () => {
       }}
     >
       <Header urlAvatarImage={avatarPixelArt} listButtons={buttons} />;
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-around",
+          alignItems: "center",
+          width: "80%",
+          margin: "auto",
+          marginTop: 10,
+        }}
+      >
+        <TitlePresentation title={"Developer"} chips={technologies} />
+        <Box
+          sx={{
+            borderRadius: "50%",
+          }}
+        >
+          <Avatar
+            alt="profile-pixel-art"
+            src={avatarPixelArt}
+            sx={{ width: "300px", height: "300px" }}
+          />
+        </Box>
+        <TitlePresentation title={"Front-end"} chips={styles} />
+      </Box>
     </Box>
   );
 };
